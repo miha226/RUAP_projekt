00f37b1b23fdc2876309a1d6072a67713729630e
miha226 <michael.srempf@gmail.com>
2022-02-21 14:25:25 +0100

    commit 3

--- EarlyStageDiabetesPredictor/EarlyStageDiabetesPredictor/Models/AzureModel.cs
+++ EarlyStageDiabetesPredictor/EarlyStageDiabetesPredictor/Models/AzureModel.cs
@@ -10,7 +10,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-
+// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Microsoft.AspNet.WebApi.Client
 namespace EarlyStageDiabetesPredictor.Models
 {
 
